80444152e4836a9056482eb432ae9f6ba90cec4e
chapi <chapimenge3@gmail.com>
2022-03-10 13:35:36 +0300

    feat: add `User` and `Link` models and fix issues

--- csec_project_catalog/authentication/models.py
+++ csec_project_catalog/authentication/models.py
@@ -1,34 +1,47 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Link(models.Model):
+    """Model for storing social links
+
+    Returns:
+        Link Object
+    """
+
     url = models.URLField(max_length=200)
     is_deleted = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(null=True, blank=True)
     deleted_by = models.ForeignKey(
-        'User', on_delete=models.SET_NULL, null=True, blank=True)
+        "User", on_delete=models.SET_NULL, null=True, blank=True
+    )
 
     def __str__(self):
-        return self.url[:50]
+        return self.url
 
 
 class User(AbstractUser):
     """User model that extends the default django user and add
     additional information's.
+
+    Returns:
+        User Object
     """
+
     phone_number = PhoneNumberField()
     is_deleted = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(null=True, blank=True)
     deleted_by = models.ForeignKey(
-        'self', on_delete=models.SET_NULL, null=True, blank=True)
+        "self", on_delete=models.SET_NULL, null=True, blank=True
+    )
     approved_by = models.ForeignKey(
-        'self', on_delete=models.SET_NULL, null=True, blank=True)
+        "self", on_delete=models.SET_NULL, null=True, blank=True
+    )
     approved_at = models.DateTimeField(null=True, blank=True)
     approved_status = models.BooleanField(default=False)
     last_login_ip = models.GenericIPAddressField(null=True, blank=True)
--- csec_project_catalog/csec_project_catalog/asgi.py
+++ csec_project_catalog/csec_project_catalog/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'csec_project_catalog.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "csec_project_catalog.settings")
 
 application = get_asgi_application()
