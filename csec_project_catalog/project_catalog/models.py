bfbaca389f66fe64a492778f4b9e0db2571cc21a
chapi <chapimenge3@gmail.com>
2022-03-15 22:41:23 +0300

    fix: some linting error fixed

--- csec_project_catalog/project_catalog/models.py
+++ csec_project_catalog/project_catalog/models.py
@@ -1,3 +1,6 @@
+# pylint: disable=too-few-public-methods
+from uuid import uuid4
+
 from authentication.models import User
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
@@ -5,40 +8,88 @@ from django.db import models
 
 
 class Rating(models.Model):
+    """Model for Rating
+
+    this model is used to store the rating of a project
+    """
+
     id = models.AutoField(primary_key=True)
     emoji = models.TextField(max_length=2048, blank=True)
     is_deleted = models.BooleanField(default=False, null=True, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True, related_name="r_creator")
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="r_creator",
+    )
     updated_at = models.DateTimeField(auto_now_add=True)
     deleted_at = models.DateTimeField(auto_now_add=True)
     deleted_by = models.ForeignKey(
-        User, on_delete=models.SET_NULL, null=True, blank=True, related_name="r_deleter")
+        User, on_delete=models.SET_NULL, null=True, blank=True, related_name="r_deleter"
+    )
+
+    def __str__(self) -> str:
+        return self.emoji
 
 
 def get_image_filepath(self, filename):
-    return 'images/' + str(self.pk) + '/profile_image.png'
+    """Method for getting image file path
+
+    Args:
+        filename (str): the image file name
+
+    Returns:
+        str: address for the image to be saved in the direcotry
+    """
+    filename = f"{uuid4()}-{filename}"
+    return "images/" + str(self.pk) + "/{filename}"
 
 
 class Image(models.Model):
+    """Model for Image Object
+
+    this models is used to save the image uploaded by the user
+    """
+
     id = models.AutoField(primary_key=True)
     image = models.ImageField(
-        max_length=255, upload_to=get_image_filepath, null=True, blank=True)
+        max_length=255, upload_to=get_image_filepath, null=True, blank=True
+    )
     is_deleted = models.BooleanField(default=False, null=True, blank=False)
     updated_at = models.DateTimeField(auto_now_add=True)
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True, related_name="i_creator")
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="i_creator",
+    )
     deleted_by = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True, related_name="i_deleter")
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="i_deleter",
+    )
     deleted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.image.url
 
 
 class Project(models.Model):
+    """Model for Project object
+
+    this model is used to store the project details
+    """
+
     id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                             on_delete=models.SET_NULL, null=True, blank=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True
+    )
     title = models.TextField(max_length=200, blank=True)
     description = models.TextField(max_length=2048, blank=True)
     project_link = models.URLField(max_length=200)
@@ -47,14 +98,31 @@ class Project(models.Model):
     updated_at = models.DateTimeField(auto_now_add=True)
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True, related_name="p_creator")
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="p_creator",
+    )
     deleted_by = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True, related_name="p_deleter")
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="p_deleter",
+    )
     deleted_at = models.DateTimeField(auto_now_add=True)
     approved_at = models.DateTimeField(auto_now_add=True)
     approved_by = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True, related_name="p_approver")
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="p_approver",
+    )
     approved_status = models.BooleanField(default=False)
-    rating = models.ManyToManyField(
-        Rating, blank=True, related_name="p_ratingss")
+    rating = models.ManyToManyField(Rating, blank=True, related_name="p_ratingss")
     images = models.ManyToManyField(Image, blank=True, related_name="p_image")
+
+    def __str__(self) -> str:
+        return self.title
