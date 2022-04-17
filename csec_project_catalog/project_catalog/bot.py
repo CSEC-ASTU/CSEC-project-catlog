173dafe37b345aca220a537ba0e4a002c35c9f1f
Natty4 <natty7kt@gmail.com>
2022-04-17 17:16:02 +0300

    fix: template error

--- csec_project_catalog/project_catalog/bot.py
+++ csec_project_catalog/project_catalog/bot.py
@@ -1,22 +1,24 @@
 import os
 import re
 
-from telegram import *
-from telegram.ext import *
+from telegram import Bot, InlineKeyboardButton, InlineKeyboardMarkup 
 
 API_KEY = os.getenv("TG_API_KEY")
 CHANNEL = os.getenv("TG_CHANNEL_ID")
 
 
 def send_to_channel(project):
-    if os.getenv("ENVIRONMENT", None) == "test":
-           return
+    if os.getenv("ENVIRONMENT", None) == "test": return
     bot = Bot(API_KEY)
+    button = [
+       [InlineKeyboardButton("View", url=project.project_link)]
+    ]
+    markup = InlineKeyboardMarkup(button)
     obj = f"""
 user : {project.user.username}
 title : {project.title}
 description : {project.description}
 project_link : {project.project_link}
 """
-    bot.send_message(CHANNEL, obj)
+    bot.send_message(CHANNEL, obj, reply_markup=markup)
     return
