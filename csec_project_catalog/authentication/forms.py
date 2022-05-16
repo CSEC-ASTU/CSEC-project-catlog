189737a86588d945c483e1952f4eb1435bf5e85a
Natty4 <natty7kt@gmail.com>
2022-05-16 10:33:55 +0300

    fix: added some missing field values for registration

--- csec_project_catalog/authentication/forms.py
+++ csec_project_catalog/authentication/forms.py
@@ -33,7 +33,7 @@ class UserRegistrationForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ("first_name", "last_name", "username", "phone_number", "email")
+        fields = ("first_name", "last_name", "username", "phone_number", "email", "gender", "birthdate")
 
     def clean(self):
         cleaned_data = super(UserRegistrationForm, self).clean()
